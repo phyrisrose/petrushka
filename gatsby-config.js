13aba2dec051ec3b791e980b44fb4319ce72bba1
Anna <phyrisrose@gmail.com>
2022-03-12 10:47:05 -0700

    installing gatsby-plugin-layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
+    "gatsby-plugin-layout",
     "gatsby-plugin-material-ui",
   ] /** @todo not sure we actually need this */,
 };
