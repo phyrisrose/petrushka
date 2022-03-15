b0e2a6b8175f2a075e14c42102565a2ade462194
Anna <phyrisrose@gmail.com>
2022-03-15 11:28:24 -0600

    trying to pull roboto in through npm

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,19 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "@emotion/styled";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme";
 
-const MainLayout = styled.div`
+const MainLayout = styled.main`
   margin: 0 auto;
   max-width: 650px;
   padding: 0 1rem;
+  font-family: "Roboto, sans-serif, serif";
 `;
 
 export default function Layout({ children }) {
