4c000a76447f87bc3ccacd7840267ab8232525c1
Anna <phyrisrose@gmail.com>
2022-03-25 12:14:45 -0600

    playing around with MUI Stack

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,22 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import { какой_нибудь_предок } from "src/content";
 
 const IndexPage = () => {
   return (
     <Container maxWidth="sm">
       <Box>
-        {какой_нибудь_предок.map((verse) => (
-          <p>
-            {verse.map((line) => (
-              <Typography variant="body1">{line}</Typography>
-            ))}
-          </p>
-        ))}
+        <Stack spacing={2}>
+          {какой_нибудь_предок.map((verse) => (
+            <div>
+              {verse.map((line) => (
+                <Typography variant="body1">{line}</Typography>
+              ))}
+            </div>
+          ))}
+        </Stack>
       </Box>
     </Container>
   );
